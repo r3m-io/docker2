e2b0d2695ed07f01268e72f85f913f72855d93bd
remco van der Velde <remco.pc@outlook.com>
2023-12-13 00:00:09 +0100

    init

---
+++ enter.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker exec -it docker-12-core /bin/bash
---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "TERM=xterm-256color" >> ~/.bashrc
+echo "mkdir -p /Application/Public" >> ~/.bashrc
+echo "mkdir -p /Application/Bin" >> ~/.bashrc
+echo "composer install -n" >> ~/.bashrc
+echo "cp /Application/vendor/r3m_io/framework/Bin/R3m.php /Application/Bin/R3m.php" >> ~/.bashrc
+echo "php /Application/Bin/R3m.php bin r3m" >> ~/.bashrc
+#echo "wat install r3m_io/node" >> ~/.bashrc
+#echo "wat install r3m_io/event" >> ~/.bashrc
+#echo "wat install r3m_io/config" >> ~/.bashrc
+#echo "wat install r3m_io/autoload" >> ~/.bashrc
+#echo "wat install r3m_io/log" >> ~/.bashrc
+#echo "wat install r3m_io/host" >> ~/.bashrc
+#echo "wat init --url=/mnt/Vps2/Data/Init/Init" >> ~/.bashrc
+echo "chown root:root /Application/start.sh" >> ~/.bashrc
+echo "chown root:root /Application/enter.sh" >> ~/.bashrc
+echo "chown root:root /Application/install.dev.sh" >> ~/.bashrc
+echo "chown root:root /Application/install.prod.sh" >> ~/.bashrc
+echo "chown root:root /Application/reinstall.dev.sh" >> ~/.bashrc
+echo "chown root:root /Application/reinstall.prod.sh" >> ~/.bashrc
+echo "r3m info all" >> ~/.bashrc
