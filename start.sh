994df289365ac578bda8923636169154d107bc16
remco van der Velde <remco.pc@outlook.com>
2023-12-13 16:13:24 +0100

    Update dependencies and refactor bash script
    This commit adds five new r3m_io libraries as dependencies in composer.json, and updates corresponding versions in composer.lock. The bash startup script 'start.sh' has been refactored with updated commands for new dependencies and removed outdated ones. It also includes changes to '.gitignore' to include the 'Bin' directory.

--- start.sh
+++ start.sh
@@ -4,13 +4,16 @@ echo "mkdir -p /Application/Public" >> ~/.bashrc
 echo "mkdir -p /Application/Bin" >> ~/.bashrc
 echo "composer install -n" >> ~/.bashrc
 echo "cp /Application/vendor/r3m_io/framework/Bin/R3m.php /Application/Bin/R3m.php" >> ~/.bashrc
-echo "php /Application/Bin/R3m.php bin r3m" >> ~/.bashrc
-echo "r3m install r3m_io/node" >> ~/.bashrc
-echo "r3m install r3m_io/event" >> ~/.bashrc
-echo "r3m install r3m_io/config" >> ~/.bashrc
-echo "r3m install r3m_io/autoload" >> ~/.bashrc
-echo "r3m install r3m_io/log" >> ~/.bashrc
-echo "r3m install r3m_io/host" >> ~/.bashrc
+echo "php /Application/Bin/R3m.php bin application" >> ~/.bashrc
+echo "application install r3m_io/node" >> ~/.bashrc
+echo "application install r3m_io/route" >> ~/.bashrc
+echo "application install r3m_io/event" >> ~/.bashrc
+echo "application install r3m_io/middleware" >> ~/.bashrc
+echo "application install r3m_io/output.filter" >> ~/.bashrc
+echo "application install r3m_io/config" >> ~/.bashrc
+echo "application install r3m_io/autoload" >> ~/.bashrc
+echo "application install r3m_io/log" >> ~/.bashrc
+echo "application install r3m_io/host" >> ~/.bashrc
 #echo "wat init --url=/mnt/Vps2/Data/Init/Init" >> ~/.bashrc
 echo "chown root:root /Application/start.sh" >> ~/.bashrc
 echo "chown root:root /Application/enter.sh" >> ~/.bashrc
@@ -18,4 +21,4 @@ echo "chown root:root /Application/install.dev.sh" >> ~/.bashrc
 echo "chown root:root /Application/install.prod.sh" >> ~/.bashrc
 echo "chown root:root /Application/reinstall.dev.sh" >> ~/.bashrc
 echo "chown root:root /Application/reinstall.prod.sh" >> ~/.bashrc
-echo "r3m info all" >> ~/.bashrc
+echo "application info all" >> ~/.bashrc
